e2342d0ec9e0865d9ec4526290f9b8fba5e7f63a
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-11-03 13:53:21 -0500

    modify interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,9 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export default interface Candidate {
-    readonly avatar_url: string | null,
+    readonly avatar: string | null,
     readonly name: string | null,
+    readonly username: string | null,
     readonly location: string | null,
     readonly email: string | null,
     readonly company: string | null,
-    readonly bio: string | null,
+    readonly html_url: string | null,
 }
